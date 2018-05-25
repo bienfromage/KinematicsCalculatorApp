0364b0566eb177d0efc0e603c4e52a9360c042e6
traweeka18 <100codemaster@gmail.com>
2018-05-25 12:47:45 -0400

    added next button

--- app/src/main/java/com/example/android/kinematicscalculator/OutputActivity.java
+++ app/src/main/java/com/example/android/kinematicscalculator/OutputActivity.java
@@ -1,17 +1,29 @@
 package com.example.android.kinematicscalculator;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class OutputActivity extends AppCompatActivity {
 
     private TextView outputTextView;
+    private Button restartButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_output);
+
+        restartButton = findViewById(R.id.restartButton);
+        restartButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(OutputActivity.this,StartActivity.class));
+            }
+        });
 
         outputTextView = findViewById(R.id.output);
         int equationNumber = CalculatorData.chooseEquation();
